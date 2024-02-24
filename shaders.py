6fa5acabc12eca964cf8733f0808da9fe081f7ac
AsinoEsel <lerefff@gmail.com>
2024-02-24 12:02:40 +0100

    Move shaders to /shaders, add more shaders for different openGL versions

--- shaders.py
+++ shaders.py
@@ -6,11 +6,11 @@ from window_manager_setup import SCREEN_WIDTH, SCREEN_HEIGHT
 TRIANGLE_STRIP = moderngl.TRIANGLE_STRIP
 
 def get_context() -> moderngl.Context:
-    return moderngl.create_context()
+    return moderngl.create_context(require=300)
 
 def get_shader_program(ctx: moderngl.Context) -> moderngl.Program:
-    with open("quad_vertex_shader310.vert") as v: vert_shader = v.read()
-    with open("crt_fragment_shader310.frag") as f: frag_shader = f.read()
+    with open("shaders/quad_vertex_shader300es.vert") as v: vert_shader = v.read()
+    with open("shaders/crt_fragment_shader300es.frag") as f: frag_shader = f.read()
     program = ctx.program(vertex_shader=vert_shader, fragment_shader=frag_shader)
     program['_MainTex'] = 0
     program['_Curvature'] = 8.0
