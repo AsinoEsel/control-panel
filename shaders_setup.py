33d39bb56314159098b76bdee63f8599422f0f87
AsinoEsel <lerefff@gmail.com>
2024-02-27 18:37:16 +0100

    Add Ghost shader uniforms

--- shaders_setup.py
+++ shaders_setup.py
@@ -1,12 +1,7 @@
 from window_manager_setup import RENDER_SIZE, RENDER_HEIGHT
 
-crt_shader_uniforms = {
+downscale_uniforms = {
     '_MainTex': 0,
-    '_Curvature': 8.0,
-    '_VignetteWidth': 40.0,
-    '_ScreenParams': RENDER_SIZE,
-    '_ScanlineHeight': 2.0 / RENDER_HEIGHT,
-    '_ScanlineStrength': 1.2,
 }
 
 threshold_shader_uniforms = {
@@ -21,26 +16,40 @@ blur_shader_uniforms = {
     '_KernelSize': 4*sigma+1,
 }
 
+ghost_shader_uniforms = {
+    '_MainTex': 1,
+    '_SecondaryTex': 2,
+    '_FadeIntensity': 0.9,
+    '_GhostInfluence': 0.9,
+}
+
 add_shader_uniforms = {
     '_MainTex': 0,
-    '_SecondaryTex': 1,
-    '_Influence': (2.0, 1.0, 1.0, 0.0),
+    '_SecondaryTex': 2,
+    '_Influence': (1.0, .25, .25, 0.0), # TODO: Does this do anything?
+    #'_Influence': (2.0, 1.0, 1.0, 0.0),
+}
+
+crt_shader_uniforms = {
+    '_MainTex': 0,
+    '_Curvature': 8.0,
+    '_VignetteWidth': 40.0,
+    '_ScreenParams': RENDER_SIZE,
+    '_ScanlineHeight': 2.0 / RENDER_HEIGHT,
+    '_ScanlineStrength': 1.2,
 }
 
 to_bgra_uniforms = {
     '_MainTex': 0,
 }
 
-downscale_uniforms = {
-    '_MainTex': 0,
-}
-
 shader_params: dict[str: tuple[str, str, dict]] = {
-    "CRT": ("shaders/quad.vert", "shaders/crt.frag", crt_shader_uniforms),
+    "Downscale": ("shaders/quad.vert", "shaders/downscale.frag", downscale_uniforms),
     "Threshold": ("shaders/quad.vert", "shaders/threshold.frag", threshold_shader_uniforms),
     "Blur_H": ("shaders/quad.vert", "shaders/blur_h.frag", blur_shader_uniforms),
     "Blur_V": ("shaders/quad.vert", "shaders/blur_v.frag", blur_shader_uniforms),
+    "Ghost": ("shaders/quad.vert", "shaders/ghost.frag", ghost_shader_uniforms),
     "Add": ("shaders/quad.vert", "shaders/add.frag", add_shader_uniforms),
+    "CRT": ("shaders/quad.vert", "shaders/crt.frag", crt_shader_uniforms),
     "To_BGRA": ("shaders/quad.vert", "shaders/to_bgra.frag", to_bgra_uniforms),
-    "Downscale": ("shaders/quad.vert", "shaders/downscale.frag", downscale_uniforms),
 }
