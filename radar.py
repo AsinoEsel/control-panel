8691bafd5df476f2b058ce5607962094dbc46f3f
AsinoEsel <lerefff@gmail.com>
2024-02-27 16:26:26 +0100

    Merge gameobject.py into radar.py

--- radar.py
+++ radar.py
@@ -1,8 +1,40 @@
 import pygame
 import sys
 import math
-from gameobject import GameObject
 import pygame.gfxdraw
+
+class GameObject:
+    def __init__(self, pos, last_hit=0, visible=False):
+        self.pos = pos
+        self.last_hit = last_hit
+        self.visible = visible
+
+    @classmethod
+    def create_random(cls, center, radius):
+        import random
+        angle = random.uniform(0, 2 * math.pi)
+        rand_radius = random.random() * radius
+        x = center[0] + rand_radius * math.cos(angle)
+        y = center[1] + rand_radius * math.sin(angle)
+        pos = (x, y)
+        return cls(pos)
+
+    @classmethod
+    def create_from_png(cls, image_path):
+        img = pygame.image.load(image_path)
+        img_size = img.get_size()
+        
+        if img_size != (800, 600):
+            print("Warning: Image is not 800x600!")
+        
+        objects = []
+        for x in range(img_size[0]):
+            for y in range(img_size[1]):
+                r, g, b, _ = img.get_at((x, y))
+                if (r, g, b) == (255, 0, 0):
+                    objects.append(cls((x, y)))
+        return objects
+
 
 class Radar:
     BLACK = (5, 10, 5)
