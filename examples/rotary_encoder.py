8b36efda0996629dda5379312b031edf994ba386
Maximilian W. Gotthardt <senexger@gmail.com>
2024-02-29 20:53:41 +0100

    Try to fix rotary encoder example

--- examples/rotary_encoder.py
+++ examples/rotary_encoder.py
@@ -21,10 +21,9 @@ def clkClicked(channel):
         global counter
         global step
  
-        clkState = GPIO.input(CLK_PIN)
         dtState = GPIO.input(DT_PIN)
  
-        if clkState == 0 and dtState == 1:
+        if dtState == 1:
                 counter = counter + step
                 print ("Counter ", counter)
  
@@ -33,20 +32,19 @@ def dtClicked(channel):
         global step
  
         clkState = GPIO.input(CLK_PIN)
-        dtState = GPIO.input(DT_PIN)
          
-        if clkState == 1 and dtState == 0:
+        if clkState == 1:
                 counter = counter - step
                 print ("Counter ", counter)
  
 def swToggle(channel):
         global toggle
-        toggle = not paused
-        print ("Paused ", toggle)             
+        toggle = not toggle
+        print ("Paused ", toggle)
                  
-GPIO.add_event_detect(CLK_PIN, GPIO.FALLING, callback=clkClicked, bouncetime=1)
-GPIO.add_event_detect(DT_PIN, GPIO.FALLING, callback=dtClicked, bouncetime=1)
-GPIO.add_event_detect(SW_PIN, GPIO.FALLING, callback=swToggle, bouncetime=1)
+GPIO.add_event_detect(CLK_PIN, GPIO.FALLING, callback=clkClicked, bouncetime=50)
+GPIO.add_event_detect(DT_PIN, GPIO.FALLING, callback=dtClicked, bouncetime=50)
+GPIO.add_event_detect(SW_PIN, GPIO.FALLING, callback=swToggle, bouncetime=50)
 
 if __name__ == "__main__":
     print("turn the wheel")
