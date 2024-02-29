df5d5ffdd2ee4c4d84bebd51186500e2d69d00f0
Maximilian W. Gotthardt <senexger@gmail.com>
2024-02-29 21:02:23 +0100

    New rotary encoder works :)

--- examples/rotary_encoder2.py
+++ examples/rotary_encoder2.py
@@ -1,8 +1,8 @@
 from pyky040 import pyky040
 
 # Define your callback
-def my_callback(scale_position):
-    print('Hello world! The scale position is {}'.format(scale_position))
+def callback(scale_position):
+    print(scale_position)
 
 # Init the encoder pins
 my_encoder = pyky040.Encoder(CLK=17, DT=18, SW=27)
@@ -11,7 +11,7 @@ my_encoder = pyky040.Encoder(CLK=17, DT=18, SW=27)
 # my_encoder = pyky040.Encoder(device='/dev/input/event0')
 
 # Setup the options and callbacks (see documentation)
-my_encoder.setup(scale_min=0, scale_max=100, step=1, chg_callback=my_callback)
+my_encoder.setup(scale_min=-100, scale_max=100, step=1, chg_callback=callback)
 
 # Launch the listener
 my_encoder.watch()
