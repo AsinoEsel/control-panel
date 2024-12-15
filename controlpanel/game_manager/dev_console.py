0d36d49632b3e6fdd15aa585a92feacc877d1d02
AsinoEsel <lerefff@gmail.com>
2024-12-15 21:12:17 +0100

    Fix issue with faulty console command cheat protection check

--- controlpanel/game_manager/dev_console.py
+++ controlpanel/game_manager/dev_console.py
@@ -202,7 +202,7 @@ class DeveloperConsole:
         if func is None:
             self.log.print(f"No command {command_name} exists in the game {self.game_manager._current_game.__class__.__name__}.", color=self.error_color, mirror_to_stdout=True)
             return
-        if getattr(func, "_is_console_command", False) and not self.game_manager.cheats_enabled:
+        if getattr(func, "_is_cheat_protected", False) and not self.game_manager.cheats_enabled:
             self.log.print(f"The command {command_name} is cheat protected.", mirror_to_stdout=True)
             return
         try:
