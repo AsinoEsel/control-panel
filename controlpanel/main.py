9febdaf61e70a0893086a219aae89044bb534f34
AsinoEsel <lerefff@gmail.com>
2025-07-01 16:05:52 +0200

    Change default launch options

--- controlpanel/main.py
+++ controlpanel/main.py
@@ -11,18 +11,18 @@ def parse_args() -> tuple[argparse.Namespace, list[str]]:
     group = parser.add_mutually_exclusive_group()
     group.add_argument('--no-gui', action='store_true',
                        help='Disable the GUI (enabled by default)')
-    group.add_argument('-w', '--windowed', action='store_true',
-                       help='Run in windowed mode (fullscreen is default)')
+    group.add_argument('-f', '--fullscreen', action='store_true',
+                       help='Run in fullscreen mode (windowed is default)')
     parser.add_argument('--height', type=int, default=540,
                         help='Height of the game window')
     parser.add_argument('--width', type=int, default=960,
                         help='Width of the game window')
-    parser.add_argument('--no-shaders', action='store_true',
-                        help='Disable shaders (shaders are enabled by default)')
+    parser.add_argument('--shaders', action='store_true',
+                        help='Enable shaders (shaders are disabled by default)')
     group.add_argument('--stretch-to-fit', action='store_true',
                        help='Stretch game to fit screen (black bars by default)')
     parser.add_argument('--load-scripts', nargs='*',
-                        help='Load script files (in gui/scripts), all by default. '
+                        help='Load script files (in controlpanel/scripts), all by default. '
                              'Alternatively, supply the filenames of the script files (or presets) to load.'
                              'A preset is a .txt file containing newline-separated script file names')
     parser.add_argument('--cheats', '-c', action='store_true', default=False,
@@ -43,8 +43,8 @@ def main():
 
     game_manager = GameManager(resolution=(args.width, args.height),
                                dev_args=unknown_args,
-                               is_fullscreen=not args.windowed,
-                               use_shaders=not args.no_shaders,
+                               is_fullscreen=args.fullscreen,
+                               use_shaders=args.shaders,
                                stretch_to_fit=args.stretch_to_fit,
                                enable_cheats=args.cheats,
                                )
