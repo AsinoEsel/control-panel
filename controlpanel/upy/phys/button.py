b215a2d6b02bd82ce14712a5ceb75d9e259cf014
AsinoEsel <lerefff@gmail.com>
2025-11-11 09:59:53 +0100

    Rewrite phys.button to only react to interrupts

--- controlpanel/upy/phys/button.py
+++ controlpanel/upy/phys/button.py
@@ -1,48 +1,49 @@
 from machine import Pin, SoftSPI, I2C
 from controlpanel.shared.base.button import BaseButton
 from .sensor import Sensor
-from controlpanel.shared.compatibility import const
+from micropython import const
 from time import ticks_ms, ticks_diff
 from controlpanel.upy.artnet import ArtNet
 
 
-_DEBOUNCE_MS = const(5)
+DEFAULT_SOFTWARE_DEBOUNCE: int = const(20)
 
 
 class Button(BaseButton, Sensor):
-    def __init__(self,
-                 _context: tuple[ArtNet, SoftSPI, I2C],
-                 _name: str,
-                 pin: int,
-                 *,
-                 update_rate_hz: float = 1.0,
-                 invert: bool = False
-                 ) -> None:
-        Sensor.__init__(self, _context[0], _name, update_rate_hz)
+    def __init__(
+            self,
+            _context: tuple[ArtNet, SoftSPI, I2C],
+            _name: str,
+            pin: int,
+            *,
+            invert: bool = False,
+            software_debounce_ms: int | None = DEFAULT_SOFTWARE_DEBOUNCE,
+    ) -> None:
+        Sensor.__init__(self, _context[0], _name, polling_rate_hz=0)
         self.pin = Pin(pin, Pin.IN, Pin.PULL_UP)
         self.pin.irq(trigger=Pin.IRQ_FALLING | Pin.IRQ_RISING, handler=self._handle_interrupt)
         self._invert = invert
         self._previous_state: bool = self.get_pressed()
-        self._last_interrupt_time: int = 0
+        self._software_debounce_ms = software_debounce_ms
+        if software_debounce_ms:
+            self._last_press_time: int = 0
+            self._last_release_time: int = 0
 
     def _handle_interrupt(self, pin: Pin) -> None:
         current_time = ticks_ms()
-        if ticks_diff(current_time, self._last_interrupt_time) < _DEBOUNCE_MS:
+        value = pin.value() ^ self._invert
+        if not self._software_debounce_ms:
+            pass
+        elif value == 1 and ticks_diff(current_time, self._last_release_time) > self._software_debounce_ms:
+            self._last_release_time = current_time
+        elif value == 0 and ticks_diff(current_time, self._last_press_time) > self._software_debounce_ms:
+            self._last_press_time = current_time
+        else:
             return
-        self._last_interrupt_time = current_time
-        current_state: bool = self.get_pressed()
-        if current_state != self._previous_state:
-            self._previous_state = current_state
-            print(f"sending trigger via interrupt {current_state}")
-            self._send_trigger_packet(int(current_state).to_bytes(1, "big"))
-
+        self._send_trigger_packet(value.to_bytes(1, "big"))
 
     def get_pressed(self) -> bool:
         return not self.pin.value() ^ self._invert
 
     async def update(self) -> None:
-        current_state: bool = self.get_pressed()
-        if current_state != self._previous_state:
-            self._previous_state = current_state
-            print(f"sending trigger {current_state}")
-            self._send_trigger_packet(int(current_state).to_bytes(1, "big"))
+        pass
