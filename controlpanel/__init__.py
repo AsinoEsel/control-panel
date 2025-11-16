40c20c802a83d0e236b3c1949f0ee65851754ad2
AsinoEsel <lerefff@gmail.com>
2025-11-16 11:39:45 +0100

    Make environ "private" to avoid namespace pollution

--- controlpanel/__init__.py
+++ controlpanel/__init__.py
@@ -1,4 +1,4 @@
-from os import environ
-environ["PYGAME_HIDE_SUPPORT_PROMPT"] = "1"
+from os import environ as _environ
+_environ["PYGAME_HIDE_SUPPORT_PROMPT"] = "1"
 from controlpanel.game_manager.games import BaseGame
 from anaconsole import console_command, Autocomplete
